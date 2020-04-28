4a396d4a4fb9103934167f02726909ed759475e6
Marcius Oliveira <marcius@fullstack.rio>
2020-04-28 11:54:34 -0300

    Removendo variável do boilerplate

--- application/sommelier/resources/wine.py
+++ application/sommelier/resources/wine.py
@@ -5,5 +5,5 @@ from sommelier.databases.models import Wine
 
 class WinesApi(Resource):
     def get(self):
-        movies = Wine.objects().to_json()
-        return Response(movies, mimetype="application/json", status=200)
+        wines = Wine.objects().to_json()
+        return Response(wines, mimetype="application/json", status=200)
